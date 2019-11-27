34f07870162c679bc33da0e99dd22308421d1eaa
n2God <kardasz.lukasz@gmail.com>
2019-11-27 22:34:15 +0100

    Session test - add 2 parameters firstname and lastname

--- src/pl/n2god/SessionServlet.java
+++ src/pl/n2god/SessionServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -15,8 +16,36 @@ import java.io.PrintWriter;
 @WebServlet("/SessionServlet")
 public class SessionServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getSession();
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user == null){
+            user = createUser(request);
+            if (user != null){
+                session.setAttribute("user", user);
+            }
+            sendResponse(response, user);
+        }
+    }
+
+    private User createUser(HttpServletRequest request) {
+        String firstName = request.getParameter("firstname");
+        String lastName = request.getParameter("lastname");
+        if (firstName == null || lastName == null){
+            return null;
+        } else return new User(firstName, lastName);
+    }
+
+    private void sendResponse(HttpServletResponse response, User user) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.write("Test sesji!");
+        writer.println("<html>");
+        writer.println("  <body>");
+        writer.println("<h1>Test Sesji</h1>");
+        if (user != null && user.getFirstName() != null && user.getLastName() != null){
+            writer.println("<div>Witaj " + user.getFirstName() + " " + user.getLastName() + "</div>");
+        } else {
+            writer.println("<div>Witaj nieznajomy!</div>");
+        }
+        writer.println("  </body>");
+        writer.println("</html>");
     }
 }
